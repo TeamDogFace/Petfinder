b9b0e15f67873dcd0b906027e1bca8208eb149ce
Tyler Ewing <tewing10@gmail.com>
2014-02-05 02:42:05 -0500

    Added more rspec tests

--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -2,25 +2,26 @@ require 'spec_helper'
 
 describe Petfinder::Pet do
 
-  before(:all) do
+  before(:each) do
     client = Petfinder::Client.new('abcde', '12345')
     @pet = client.get_pet('28432461')
   end
 
   it "should have correct name" do
-    @pet.name.should == 'Charlie'
+    expect(@pet.name).to eql('Charlie')
   end
 
-  # it "should have id from hash" do
-  #   @pet.id = 12345
-  # end
+  it "should have id from hash" do
+    expect(@pet.id).to eql('28432461')
+  end
 
-  # it "should have shelter_id from hash" do
-  #   @pet.id = 67890
-  # end
+  it "should have valid images" do
+    expect(@pet.media.count).to eql(10)
+  end
 
-  # it "should have nil sex" do
-  #   @pet.sex.should be_nil
-  # end
+  it "shoudl have correct contact" do
+    expect(@pet.contact.city).to eql('Orange')
+    expect(@pet.contact.state).to eql('CA')
+  end
 
 end
