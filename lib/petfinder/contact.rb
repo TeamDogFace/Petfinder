7657a6ec11fb5b92a52d8fa26cdac714e05b07de
Tyler Ewing <tewing10@gmail.com>
2014-02-05 19:01:56 -0500

    Checked for nulls in HTTP response

--- lib/petfinder/contact.rb
+++ lib/petfinder/contact.rb
@@ -2,6 +2,6 @@ module Petfinder
 
   class Contact
   	include HashConstructor
-    attr_accessor :address1, :address2, :city, :state, :zip, :phone, :fax, :email
+    attr_accessor :address1, :address2, :city, :state, :zip, :phone, :fax, :email, :name
   end
 end
--- lib/petfinder/pet.rb
+++ lib/petfinder/pet.rb
@@ -8,19 +8,19 @@ module Petfinder
 
     # Some of data is deeper in the XML
     def breeds=(breeds)
-      @breeds = breeds["breed"]
+      @breeds = breeds["breed"] unless breeds.nil?
     end
     
     def options=(options)
-      @options = options["option"]
+      @options = options["option"] unless options.nil?
     end
 
     def media=(media)
-      @media = media["photos"]["photo"]
+      @media = media["photos"]["photo"] unless media.nil?
     end
 
     def contact=(contact)
-      @contact = Contact.new(contact)
+      @contact = Contact.new(contact) unless contact.nil?
     end
 
   end
