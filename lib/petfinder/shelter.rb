087221a3a7dbcd4d6460529737e6ea3f9e11bebd
Tyler Ewing <tewing10@gmail.com>
2014-04-23 18:46:18 -0400

    Added in last_offset attribute for requests that return lots of records

--- lib/petfinder/shelter.rb
+++ lib/petfinder/shelter.rb
@@ -3,7 +3,7 @@ module Petfinder
   class Shelter
     include HashConstructor
     attr_accessor :id, :name, :address1, :address2, :city, :state, :zip, :country,
-     :latitude, :longitude, :phone, :fax, :email
+     :latitude, :longitude, :phone, :fax, :email, :last_offset
   end
 
 end
