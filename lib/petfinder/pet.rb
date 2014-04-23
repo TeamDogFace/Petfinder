c809ddc56b53f922ffe105147a363654f11a2372
Tyler Ewing <tewing10@gmail.com>
2014-04-23 19:25:37 -0400

    Removed lastOffset because it is in a different place in the response

--- lib/petfinder/pet.rb
+++ lib/petfinder/pet.rb
@@ -3,7 +3,7 @@ module Petfinder
   class Pet
     include HashConstructor
     attr_accessor :id, :shelter_id, :shelter_pet_id, :name, :animal, :breeds,
-      :mix, :age, :sex, :size, :last_offset, :options, :description, :last_update, :status, 
+      :mix, :age, :sex, :size, :options, :description, :last_update, :status, 
       :media, :contact
 
     # Some of data is deeper in the XML
